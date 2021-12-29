ad8d19b6da5e676c574838cd93f3a9d2facf6049
tkgka <ksh17171@gmail.com>
2021-12-29 20:34:57 +0900

    ./

--- cache/cache.ts
+++ cache/cache.ts
@@ -1,8 +1,9 @@
 //redis 시작
 //brew services start redis 
+import env from '../env';
 const redis = require('redis')
-const client = redis.createClient(6379, '127.0.0.1') //연결
-//client.auth('[password]') //인증
+const client = redis.createClient(6379, env.redis_host) //연결
+//client.auth(env.redis_pw) //인증
 client.on('error', (err) => {//에러 핸들링
   console.log('redis error ' + err)
 })
